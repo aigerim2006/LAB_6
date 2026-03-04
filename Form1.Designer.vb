b14c8d098c8e7625175a32b79d0df5b7283efc10
aigerim2006 <aigerimhalbekova@gmail.com>
2026-03-04 23:36:37 +0600

    Задание 6.1 Построение графиков функций

--- Form1.Designer.vb
+++ Form1.Designer.vb
@@ -22,10 +22,139 @@ Partial Class Form1
     'Не изменяйте ее в редакторе исходного кода.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.TextBox4 = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.875!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(204, Byte))
+        Me.TextBox1.Location = New System.Drawing.Point(143, 314)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(125, 40)
+        Me.TextBox1.TabIndex = 0
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.875!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(204, Byte))
+        Me.TextBox2.Location = New System.Drawing.Point(143, 373)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(125, 40)
+        Me.TextBox2.TabIndex = 1
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.875!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(204, Byte))
+        Me.TextBox3.Location = New System.Drawing.Point(423, 314)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(118, 40)
+        Me.TextBox3.TabIndex = 2
+        '
+        'TextBox4
+        '
+        Me.TextBox4.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.875!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(204, Byte))
+        Me.TextBox4.Location = New System.Drawing.Point(423, 373)
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.Size = New System.Drawing.Size(118, 40)
+        Me.TextBox4.TabIndex = 3
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(597, 331)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(165, 56)
+        Me.Button1.TabIndex = 4
+        Me.Button1.Text = "Построить"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(204, Byte))
+        Me.Label1.Location = New System.Drawing.Point(76, 308)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(35, 37)
+        Me.Label1.TabIndex = 5
+        Me.Label1.Text = "a"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(204, Byte))
+        Me.Label2.Location = New System.Drawing.Point(76, 373)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(35, 37)
+        Me.Label2.TabIndex = 6
+        Me.Label2.Text = "b"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(204, Byte))
+        Me.Label3.Location = New System.Drawing.Point(366, 314)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(33, 37)
+        Me.Label3.TabIndex = 7
+        Me.Label3.Text = "c"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(204, Byte))
+        Me.Label4.Location = New System.Drawing.Point(364, 373)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(35, 37)
+        Me.Label4.TabIndex = 8
+        Me.Label4.Text = "d"
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Location = New System.Drawing.Point(163, 31)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(480, 251)
+        Me.PictureBox1.TabIndex = 9
+        Me.PictureBox1.TabStop = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(12.0!, 25.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.TextBox4)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Name = "Form1"
         Me.Text = "Form1"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
 
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents PictureBox1 As PictureBox
 End Class
--- Form1.vb
+++ Form1.vb
@@ -1,3 +1,88 @@
 ﻿Public Class Form1
 
+    ' Переменные уровня класса
+    Private a, b, c, d As Double
+    Private paper As Graphics
+    Private myPen As New Pen(Color.Black)
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        PictureBox1.BackColor = Color.White
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Try
+            paper = PictureBox1.CreateGraphics()
+            DrawGraph()
+        Catch ex As Exception
+            MessageBox.Show("Ошибка ввода коэффициентов!")
+        End Try
+    End Sub
+
+    Private Sub DrawGraph()
+
+        ' Считывание коэффициентов
+        a = CDbl(TextBox1.Text)
+        b = CDbl(TextBox2.Text)
+        c = CDbl(TextBox3.Text)
+        d = CDbl(TextBox4.Text)
+
+        paper.Clear(Color.White)
+
+        Draw()
+
+    End Sub
+
+    Private Sub Draw()
+
+        Dim x, y, nextX, nextY As Double
+        Dim xPixel, yPixel, nextXPixel, nextYPixel As Integer
+
+        For xPixel = 0 To PictureBox1.Width - 2
+
+            ' 1. Перевод пикселя в координату X
+            x = ScaleX(xPixel)
+
+            ' 2. Вычисление значения функции
+            y = TheFunction(x)
+
+            ' 3. Перевод Y в пиксели
+            yPixel = ScaleY(y)
+
+            ' Следующая точка
+            nextXPixel = xPixel + 1
+            nextX = ScaleX(nextXPixel)
+            nextY = TheFunction(nextX)
+            nextYPixel = ScaleY(nextY)
+
+            ' Рисуем маленький отрезок
+            paper.DrawLine(myPen, xPixel, yPixel, nextXPixel, nextYPixel)
+
+        Next
+
+    End Sub
+
+    Private Function TheFunction(x As Double) As Double
+        Return a * x ^ 3 + b * x ^ 2 + c * x + d
+    End Function
+
+    Private Function ScaleX(xPixel As Integer) As Double
+
+        Dim xStart As Double = -5
+        Dim xEnd As Double = 5
+        Dim xScale As Double = PictureBox1.Width / (xEnd - xStart)
+
+        Return (xPixel - PictureBox1.Width / 2) / xScale
+
+    End Function
+
+    Private Function ScaleY(y As Double) As Integer
+
+        Dim yStart As Double = -5
+        Dim yEnd As Double = 5
+        Dim yScale As Double = PictureBox1.Height / (yEnd - yStart)
+
+        Return CInt(-y * yScale + PictureBox1.Height / 2)
+
+    End Function
+
 End Class
