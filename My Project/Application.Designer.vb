8fca27ab1096c7035b8366d1a38de92207609ef1
aigerim2006 <aigerimhalbekova@gmail.com>
2026-03-26 10:40:07 +0600

    Последний коммит

--- My Project/Application.Designer.vb
+++ My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.LAB_6.Form2
+            Me.MainForm = Global.LAB_6.Form1
         End Sub
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
